b1b6cbbc7a510f7b0ba2279bba4dc1ca47b5233b
beyazskorsky <hhsynalv@gmail.com>
2022-04-15 23:56:17 +0300

    Initial commit

---
+++ src/components/User.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="container">
+    <h1>Parent Component (User)</h1>
+    <p>Burası parent component yani herşeyin import edildiği component :)</p>
+    <button @click="changeName">Adımı Değiştir</button>
+    <hr>
+    <p>Child Component Üzerinden gelen Veri : {{childData}}</p>
+    <hr>
+    <div class="row">
+      <app-user-detail  :age="age" @data="childData=$event" :name="title"></app-user-detail>
+      <app-user-edit :age="age" @ageWasEdited="age=$event"></app-user-edit>
+    </div>
+  </div>
+</template>
+<script>
+  import UserDetail from "./UserDetail";
+  import UserEdit from "./UserEdit";
+  export default {
+    components : {
+      appUserDetail : UserDetail,
+      appUserEdit : UserEdit,
+    },
+    data(){
+      return {
+        title : "Hasan Hüseyin",
+        childData : "",
+        age : 25
+      }
+    },
+    methods : {
+      changeName(){
+        this.title = "Hüseyin"
+      }
+    }
+  }
+</script>
+
+<style>
+  div.container{
+    margin-top: 30px;
+    padding: 20px 40px;
+    background-color: #6a8d99;
+    border: 1px solid #666;
+  }
+  div.row {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
